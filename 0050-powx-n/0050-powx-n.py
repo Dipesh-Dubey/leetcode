8900c33949cdc7d5f6cb5037918c310d37a37288
Dipesh-Dubey <dipeshdubey2015@gmail.com>
2026-02-04 00:34:35 +0530

    Time: 0 ms (100%), Space: 19.5 MB (42.85%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        num = n
+        if num < 0:
+            return 1.0 / self.power(x, -num)
+
+        return self.power(x, num)
+        
+    def power(self,x,n):
+        if n==0:
+            return 1
+        if n%2==0:
+            return self.power(x * x, n // 2)       
+        else:
+            return x * self.power(x, n - 1)
