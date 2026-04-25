ba9fa4678877f37324c2af35b9691a81657de984
Dipesh-Dubey <dipeshdubey2015@gmail.com>
2026-04-25 14:26:31 +0530

    Time: 108 ms (53.48%), Space: 61.3 MB (55.91%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,20 +2,15 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         path = []
-        nums = []
-        for i in range(1,n+1):
-            nums.append(i)
 
-        def dfs(i,path):
+        def dfs(start,path):
             if len(path)==k:
-                res.append(path.copy())
-                return
-            if i==len(nums):
-                return
-            path.append(nums[i])
-            dfs(i+1,path)
-            path.pop()
-            dfs(i+1,path)
+                    res.append(path.copy())
+                    return
+            for num in range(start,n+1):
+                path.append(num)
+                dfs(num+1,path)
+                path.pop()
             
-        dfs(0,path)
+        dfs(1,path)
         return res
