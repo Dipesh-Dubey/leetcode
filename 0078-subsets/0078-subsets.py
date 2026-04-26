5518fd6939250c085e6e27e78d2049e1bbd857f7
Dipesh-Dubey <dipeshdubey2015@gmail.com>
2026-04-26 13:29:33 +0530

    Time: 0 ms (100%), Space: 19.5 MB (30.9%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,15 +3,12 @@ class Solution:
         res = []
         path = []
 
-        def dfs(i,path):
-            if i == len(nums):
-                res.append(path.copy())
-                return
-            
-            path.append(nums[i])
-            dfs(i+1,path)
-            path.pop()
-            dfs(i+1,path)
+        def dfs(start,path):
+            res.append(path.copy())           
+            for i in range(start,len(nums)):
+                path.append(nums[i])
+                dfs(i+1,path)
+                path.pop()      
         
         dfs(0,path)
         return res
