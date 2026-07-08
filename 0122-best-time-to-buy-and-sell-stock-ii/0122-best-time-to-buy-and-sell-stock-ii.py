a312388961ecb2eb716fc6b71134026de3dccd2e
Dipesh-Dubey <dipeshdubey2015@gmail.com>
2026-07-09 00:54:10 +0530

    Time: 39 ms (5.09%), Space: 31.9 MB (5.03%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[-1]*2 for _ in range(n)]
+
+        def dfs_knap(i, flag):
+            if i == n:
+                return 0
+
+            if dp[i][flag] != -1:
+                return dp[i][flag]
+
+            if not flag:
+                dp[i][flag] = max(-prices[i] + dfs_knap(i+1, True),dfs_knap(i+1, False))
+            else:
+                dp[i][flag] = max(prices[i] + dfs_knap(i + 1, False),dfs_knap(i + 1, True))
+                
+            return dp[i][flag]
+
+        return dfs_knap(0, False)
