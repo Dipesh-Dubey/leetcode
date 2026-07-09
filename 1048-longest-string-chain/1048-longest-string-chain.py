538c18dedc28a2e9d675d8288f44d8dca4b4f6fa
Dipesh-Dubey <dipeshdubey2015@gmail.com>
2026-07-10 02:46:08 +0530

    Time: 2605 ms (5.12%), Space: 95.5 MB (5.13%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,33 +1,39 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key = lambda x:len(x))
+        print(words)
         n = len(words)
-        dp = [1]*n
-        def comp(w1, w2):
-            if len(w2) - len(w1) != 1:
-                return False
-            i = j = 0
-            mismatch = 0      
-            while i < len(w1) and j < len(w2):
-                if w1[i] == w2[j]:
-                    i += 1
-                    j += 1
-                else:
-                    mismatch += 1
-                    j += 1
-                    if mismatch > 1:
-                        return False       
-            return True
-        words.sort(key=len)
-        for ind in range(n):
-            for prev_ind in range(ind):
-                if comp(words[prev_ind],words[ind]):
-                    if dp[prev_ind]+1 > dp[ind]:
-                        dp[ind] = dp[prev_ind]+1
-        
-        maxi = 0
-        for i in range(n):
-            if dp[i]>maxi: 
-                maxi = dp[i]
-        
-        return maxi
+        dp = [[-1] * (n + 1) for _ in range(n)]
 
+        def isPred(shorter, longer):
+            if len(longer) != len(shorter) + 1:
+                return False
+
+            p1 = p2 = 0
+
+            while p1 < len(shorter) and p2 < len(longer):
+                if shorter[p1] == longer[p2]:
+                    p1 += 1
+                    p2 += 1
+                else:
+                    p2 += 1
+
+            return p1 == len(shorter)
+
+        def dfs(i, prev):
+            if i == n:
+                return 0
+
+            if dp[i][prev + 1] != -1:
+                return dp[i][prev + 1]
+
+            if prev == -1 or isPred(words[prev], words[i]):
+                dp[i][prev + 1] = max(1 + dfs(i+1, i), dfs(i+1, prev))
+
+            else:
+                dp[i][prev + 1] = dfs(i + 1, prev)
+
+            return dp[i][prev + 1]
+
+        return dfs(0, -1)
+    
